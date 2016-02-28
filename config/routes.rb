f987f9e5915fd6cb296ea671ee14eb52c5ec4654
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2016-02-28 18:16:19 +0900

    Facebook OAuth

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,15 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'root#index'
 
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'omniauth_callbacks' },
+    skip: [ :sessions, :registrations, :password ]
+
+  devise_scope :user do
+    get 'signin',  to: 'user#login',              as: :new_user_session
+    get 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
