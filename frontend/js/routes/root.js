8527a823f289a6078336e21aa1e3432d0733f5a3
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2016-03-19 21:23:02 +0900

    Carousel をモジュール化

--- frontend/js/routes/root.js
+++ frontend/js/routes/root.js
@@ -1,9 +1,11 @@
-import '../lib/carousel'
+import Carousel from '../lib/carousel'
 
 export default function(req) {
   switch (req.action) {
 
     case 'index': return function() {
+      var carousel = new Carousel('#mainCarousel');
+      carousel.start();
     }
 
   }
