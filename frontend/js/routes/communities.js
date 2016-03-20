5ef5b71285c35ab149f851329ea6019358040fda
yossy <mi@mi.local>
2016-03-20 13:53:56 +0900

    SP 一緒に遊ぶボタン固定　js

--- frontend/js/routes/communities.js
+++ frontend/js/routes/communities.js
@@ -5,32 +5,45 @@ export default function(req) {
 
     case 'show': return function() {
 
-      console.log('ss');
-      // PC サイドバ―固定
-      var $eventAction = $("#eventAction");
-      var $main = $(".eventBody__left");
-      var eventAction_offset_top = $eventAction.offset().top;
-      var main_offset_top = $main.offset().top;
-      var sub_scroll = main_offset_top + $main.outerHeight(true) - $eventAction.outerHeight(true);
+      if ( ! req.sp ) {
+        // PC サイドバ―固定
+        var $eventAction = $("#eventAction");
+        var $main = $(".eventBody__left");
+        var eventAction_offset_top = $eventAction.offset().top;
+        var main_offset_top = $main.offset().top;
+        var sub_scroll = main_offset_top + $main.outerHeight(true) - $eventAction.outerHeight(true);
 
-      if ($eventAction.outerHeight(true) + eventAction_offset_top < $main.outerHeight(true) + main_offset_top) {
+        if ($eventAction.outerHeight(true) + eventAction_offset_top < $main.outerHeight(true) + main_offset_top) {
+          $(window).scroll(function () {
+            var ws = $(window).scrollTop();
+            if (ws > sub_scroll) {
+              $eventAction.css({position:'fixed', top: sub_scroll - ws + 'px'});
+            } else if(ws > eventAction_offset_top) {
+              $eventAction.css({position:'fixed', top: '0px'});
+            } else {
+              $eventAction.css({position:'relative', top: '0px'});
+            }
+          })
+        }
+      }
+      // スマホ
+      else {
+        // SP 一緒に遊ぶボタン固定
+        var $eventActionScroll = $(".eventActionScroll");
+        var $eventArea = $(".event");
+        var eventArea_offset_top = $eventArea.offset().top;
+        var windowHeight = $(window).height();
+        var sub_scroll = eventArea_offset_top + $eventArea.outerHeight(true) - $eventActionScroll.outerHeight(true) - windowHeight;
+
         $(window).scroll(function () {
           var ws = $(window).scrollTop();
           if (ws > sub_scroll) {
-            $eventAction.css({position:'fixed', top: sub_scroll - ws + 'px'});
-          } else if(ws > eventAction_offset_top) {
-            $eventAction.css({position:'fixed', top: '0px'});
+            $eventActionScroll.css({position:'absolute', bottom: '0px'});
           } else {
-            $eventAction.css({position:'relative', top: '0px'});
+            $eventActionScroll.css({position:'fixed', bottom: '0px'});
           }
         })
       }
-
-      // SP 一緒に遊ぶボタン固定
-      var $eventActionScroll = $(".eventActionScroll");
-      var $eventArea = $(".event");
-      // var eventActionScroll_offset_top = $eventActionScroll.offset().top;
-      // var eventArea_offset_top = $eventArea.offset().top;
     }
   }
 }
