6b9e7d93abe3c60be8567501f64bacdc1f44bad0
yossy <mi@mi.local>
2016-03-22 12:37:21 +0900

    PC フォトギャラリーjs 追加

--- frontend/js/routes/communities.js
+++ frontend/js/routes/communities.js
@@ -5,6 +5,67 @@ export default function(req) {
 
     case 'show': return function() {
 
+      function photoGallary() {
+        // 写真ギャラリー
+        var $main = $(".main");
+        var $thumbnails = $(".thumbnails");
+        var $mainItems = $main.find(".mainItem");
+        var $thumbnailsItems = $thumbnails.find(".thumbnailsItem");
+        var mainCount = 0;
+        var pageCount = 0;
+        var mainLimit = $mainItems.size();
+        var thumbnailsLimit = $thumbnailsItems.size();
+        var thumbnailPageSize = 5;
+        var pageMax =  Math.ceil(thumbnailsLimit / thumbnailPageSize);
+        var pageWidth = 100 / thumbnailsLimit * thumbnailPageSize;
+        $thumbnailsItems.first().addClass("current");
+
+        function showPhoto(n) {
+          $main.css("transform", "translateX( -" + (100 / mainLimit * n) + "%)");
+          $thumbnailsItems.removeClass("current");
+          $thumbnailsItems.eq(n).addClass("current");
+          mainCount = n;
+
+          pageCount = Math.floor(n / thumbnailPageSize);
+          $thumbnails.css("transform", "translateX( -" + ( pageWidth * pageCount ) + "%)");
+        }
+
+        $(".mainBox .next").on('click', function() {
+          if(mainCount < mainLimit - 1) {
+            showPhoto(mainCount + 1);
+          }
+        });
+
+        $(".mainBox .prev").on('click', function() {
+          if(mainCount > 0) {
+            showPhoto(mainCount - 1);
+          }
+        });
+
+        $thumbnailsItems.on('click', function() {
+          // サムネイルがクリックされた番号
+          var index = $thumbnailsItems.index(this);
+          showPhoto(index);
+        });
+
+        $(".thumbnailsBox .next").on('click', function() {
+
+          if(pageCount < pageMax -1 ) {
+            $thumbnails.css("transform", "translateX( -" + ( pageWidth * ( pageCount + 1) ) + "%)");
+            pageCount++;
+          }
+        });
+
+        $(".thumbnailsBox .prev").on('click', function() {
+          if(pageCount > 0 ) {
+            $thumbnails.css("transform", "translateX( -" + ( pageWidth * ( pageCount - 1) ) + "%)");
+            pageCount--;
+          }
+        });
+      }
+      photoGallary();
+
+
       if ( ! req.sp ) {
         // PC サイドバ―固定
         var $eventAction = $("#eventAction");
