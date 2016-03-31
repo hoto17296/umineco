ec6e6bbf3dcc57a7ca4ebd1e5abe5fba65798bf7
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2016-03-31 23:03:41 +0900

    申し込み状況によってボタンを出し分ける

--- app/models/sailing.rb
+++ app/models/sailing.rb
@@ -4,4 +4,15 @@ class Sailing < ActiveRecord::Base
   has_many :participants
   has_many :users, through: :participants
   has_many :comments
+
+  def participant?(user)
+    return false unless user.instance_of? User
+    participants.inject(false) {|v, p| v || p.user.id == user.id }
+  end
+
+  # 最大参加人数(仮)
+  def max
+    10
+  end
+
 end
