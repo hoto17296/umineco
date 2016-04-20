9fc1df09c1e615af4077d5d3f7af541bfaeaeea6
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2016-04-20 22:39:07 +0900

    yml データを入れるカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408092505) do
+ActiveRecord::Schema.define(version: 20160420133054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160408092505) do
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "page_data"
   end
 
   create_table "feeds", force: :cascade do |t|
