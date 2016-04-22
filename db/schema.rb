68a3a972ef38f0935fe5c0dcc2ca7ed00e5e1179
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2016-04-22 15:25:25 +0900

    comments.rating カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420133054) do
+ActiveRecord::Schema.define(version: 20160422060856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160420133054) do
     t.integer  "user_id",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "rating"
   end
 
   add_index "comments", ["sailing_id"], name: "index_comments_on_sailing_id", using: :btree
